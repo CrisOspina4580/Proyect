40f2088f9486d1783368456c55d3cad451ab5f20
CrisOspina4580 <crisospina321@gmail.com>
2024-02-02 18:09:38 -0500

    2.0 version

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+
+import { PrincipalComponent } from './modulos/principal.component';
+import { DasboardComponent } from './modulos/dasboard/dasboard.component';
+import { LoginComponent } from './modulos/login/login.component';
+
+
+const routes: Routes = [
+  {
+    path: '', component: PrincipalComponent,
+     children: [
+      { path: 'dasboard', component: DasboardComponent },
+      { path: '', redirectTo: '/dasboard', pathMatch: 'full'}
+    ]
+  },
+  { path: 'login', component: LoginComponent },
+];
+
+//const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import {HttpClientModule} from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './estructura/header/header.component';
+import { NavComponent } from './estructura/nav/nav.component';
+import { FooterComponent } from './estructura/footer/footer.component';
+import { PrincipalComponent } from './modulos/principal.component';
+import { DasboardComponent } from './modulos/dasboard/dasboard.component';
+import { LoginComponent } from './modulos/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavComponent,
+    FooterComponent,
+    PrincipalComponent,
+    DasboardComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
